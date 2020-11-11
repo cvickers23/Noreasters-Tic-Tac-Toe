566e9a141f88f6a08137172a36b016b44254f3cc
JNozaki <64598286+JNozaki@users.noreply.github.com>
2020-11-11 16:58:49 -0500

    creating unittest class for Tic Tac Toe

---
+++ test_tic_tac_toe.py
@@ -0,0 +1,9 @@
+#import libraries
+import main
+import unittest
+
+class Test_TestTicTacToe(unittest.TestCase):
+    def Test_Symbol_Selection(self):
+        self.assertEqual(main.choose_token(),)
+
+    def Test_
